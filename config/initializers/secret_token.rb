e2ac9d8a4e894ba26f0e5d261bd320907cdb1b25
Mark Maximus Muskardin <mark.muskardin@gmail.com>
2013-03-20 14:04:31 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoundationPlayground::Application.config.secret_token = 'b4a24d872bebee6cb59d0316cfafdd1b3fbb86dfc3bce874fb6320b850888ad23e5e6be9a2d02bce0532ada47e9b3756852cbac3b3046f88c46407b54555dfc5'
